0772afb450d9de6cd15d85268e65a37104899f64
Paul Asjes <p.asjes@gmail.com>
2023-03-16 11:16:02 +0100

    Removed netlify config, added vercel

--- svelte.config.js
+++ svelte.config.js
@@ -43,18 +43,9 @@ const config = {
 		})
 	],
 
-	// Docs: https://github.com/sveltejs/kit/blob/master/packages/adapter-netlify/README.md
 	kit: {
 		adapter: adapter({
-			split: false,
-
-			// nov 2022
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions. however, also uses esbuild, which as of nov 2022 has a bug on netlify
-			// https://github.com/sveltejs/kit/issues/7839#issuecomment-1328605300
-
-			// dec 2022 - moved back to true since we're using esbuild again
-			edge: true,
+			runtime: 'edge',
 		})
 	}
 };
