ff6264920550313fe3358993477aca67247d5380
Paul Asjes <p.asjes@gmail.com>
2023-03-21 20:41:43 +0100

    Hopefully a fix for the prism bug

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,10 +6,10 @@
   <h1 class="mb-4 text-3xl font-bold tracking-tight md:text-5xl">About me</h1>
   <div class="space-y-4">
     <p>
-      Hello, I'm Paul Asjes. I work as a Developer Advocacy Engineering Manager at Stripe. My main focus is raising awareness, being the voice of the developer and giving internal teams what they need to build great products.
+      Hello, I'm Paul Asjes. I work as a Developer Advocacy Engineering Manager at Stripe. I focus on raising awareness, being the voice of the developer and giving internal teams what they need to build great products.
     </p>
     <p>
-      Before shifting to Developer Relations I was a product engineer for 10+ years, working at Tumblr and various start ups over the years. I began as a Flash developer back in the stone age of the internet, now preferring to work in the JavaScript ecosystem.
+      Before shifting to Developer Relations I was a product engineer for 10+ years, working at Tumblr and various start ups over the years. I started my career as a Flash developer back in the stone age of the internet, now preferring to work in the JavaScript ecosystem.
     </p>
   </div>  
 </section>
@@ -27,17 +27,17 @@
   <div class="m-4">
     <h2 class="text-xl font-bold mb-2">Website</h2>
     <ul class="list-disc">
-      <li>Svelte</li>
-      <li>Swyxkit</li>
-      <li>Vercel</li>
+      <li><a href="https://svelte.dev/" class="dark:text-blue-400">Svelte</a></li>
+      <li><a href="https://swyxkit.netlify.app/" class="dark:text-blue-400">Swyxkit</a></li>
+      <li><a href="https://vercel.com/" class="dark:text-blue-400">Vercel</a></li>
     </ul>  
   </div>
   
   <div class="m-4">    
     <h2 class="text-xl font-bold mb-2">Software</h2>
     <ul class="list-disc">
-      <li>VSCode (with Dracula theme)</li>
-      <li>iTerm2</li>
+      <li>VSCode (with <a href="https://draculatheme.com/visual-studio-code" class="dark:text-blue-400">Dracula</a> theme)</li>
+      <li><a href="https://iterm2.com/" class="dark:text-blue-400">iTerm2</a></li>
       <li><a href="https://bear.app/" class="dark:text-blue-400">Bear notes</a></li>
     </ul>
   </div>
