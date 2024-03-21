95e1ebe4368fcb5795c8d58d60097a51893f3b1d
Paul Asjes <paul.asjes@workos.com>
2024-03-21 09:10:03 +0100

    Update about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,50 +1,75 @@
 <script>
-  import { MY_TWITTER_HANDLE } from '$lib/siteConfig';
+	import { MY_TWITTER_HANDLE } from '$lib/siteConfig';
 </script>
 
 <section class="flex justify-center">
-  <div class="m-4 w-[150px] h-[150px] rounded-full sm:w-[250px] sm:h-[250px] relative">
-    <img src="/about-profile.jpg" class="rounded-full filter p-1 bg-gradient-to-r from-white to-blue-400" alt="Paul Asjes" />
-  </div>
+	<div class="m-4 w-[150px] h-[150px] rounded-full sm:w-[250px] sm:h-[250px] relative">
+		<img
+			src="/about-profile.jpg"
+			class="rounded-full filter p-1 bg-gradient-to-r from-white to-blue-400"
+			alt="Paul Asjes"
+		/>
+	</div>
 </section>
 
-<section class="flex flex-col items-start justify-center max-w-5xl px-4 mx-auto sm:px-8 py-8 text-black dark:text-white">
-  <h1 class="mb-4 text-3xl font-bold tracking-tight md:text-5xl">About me</h1>
-  <div class="space-y-4">
-    <p>
-      Hello, I'm Paul Asjes. I work as a Developer Advocacy Engineering Manager at Stripe. I focus on raising awareness, being the voice of the developer and giving internal teams what they need to build great products.
-    </p>
-    <p>
-      Before shifting to Developer Relations I was a product engineer for 10+ years, working at Tumblr and various start ups over the years. I started my career as a Flash developer back in the stone age of the internet, now preferring to work in the JavaScript ecosystem.
-    </p>
-  </div>  
+<section
+	class="flex flex-col items-start justify-center max-w-5xl px-4 mx-auto sm:px-8 py-8 text-black dark:text-white"
+>
+	<h1 class="mb-4 text-3xl font-bold tracking-tight md:text-5xl">About me</h1>
+	<div class="space-y-4">
+		<p>
+			Hello, I'm Paul Asjes. I work as a Developer Experience Engineer at WorkOS. I focus on raising
+			awareness, being the voice of the developer and giving internal teams what they need to build
+			great products.
+		</p>
+		<p>
+			Before shifting to Developer Relations I was a product engineer for 10+ years, working at
+			Tumblr and various start ups over the years. I started my career as a Flash developer back in
+			the stone age of the internet, now preferring to work in the JavaScript ecosystem.
+		</p>
+	</div>
 </section>
 
-<section class="flex flex-col items-start justify-center max-w-5xl px-4 mx-auto sm:px-8 py-8 text-black dark:text-white">
-  <h1 class="mb-4 text-3xl font-bold tracking-tight md:text-5xl">Contact</h1>
-  <p>
-    If you want to get a hold of me, the best way is via <a href={`https://twitter.com/${MY_TWITTER_HANDLE}`} class="dark:text-blue-400">Twitter</a> or you can <a href="mailto:hello@asjes.dev" class="dark:text-blue-400">email</a> me directly. If inquiring about speaking at a conference, please include the dates, location, type of conference and talk type in your message.
-  </p>
+<section
+	class="flex flex-col items-start justify-center max-w-5xl px-4 mx-auto sm:px-8 py-8 text-black dark:text-white"
+>
+	<h1 class="mb-4 text-3xl font-bold tracking-tight md:text-5xl">Contact</h1>
+	<p>
+		If you want to get a hold of me, the best way is via <a
+			href={`https://twitter.com/${MY_TWITTER_HANDLE}`}
+			class="dark:text-blue-400">Twitter</a
+		>
+		or you can <a href="mailto:hello@asjes.dev" class="dark:text-blue-400">email</a> me directly. If
+		inquiring about speaking at a conference, please include the dates, location, type of conference
+		and talk type in your message.
+	</p>
 </section>
 
-<section class="flex flex-col items-start justify-center max-w-5xl w-full px-4 mx-auto sm:px-8 py-8 text-black dark:text-white">
-  <h1 class="mb-4 text-3xl font-bold tracking-tight md:text-5xl">Uses</h1>
-  
-  <div class="m-4">
-    <h2 class="text-xl font-bold mb-2">Website</h2>
-    <ul class="list-disc">
-      <li><a href="https://svelte.dev/" class="dark:text-blue-400">Svelte</a></li>
-      <li><a href="https://swyxkit.netlify.app/" class="dark:text-blue-400">Swyxkit</a></li>
-      <li><a href="https://vercel.com/" class="dark:text-blue-400">Vercel</a></li>
-    </ul>  
-  </div>
-  
-  <div class="m-4">    
-    <h2 class="text-xl font-bold mb-2">Software</h2>
-    <ul class="list-disc">
-      <li>VSCode (with <a href="https://draculatheme.com/visual-studio-code" class="dark:text-blue-400">Dracula</a> theme)</li>
-      <li><a href="https://iterm2.com/" class="dark:text-blue-400">iTerm2</a></li>
-      <li><a href="https://bear.app/" class="dark:text-blue-400">Bear notes</a></li>
-    </ul>
-  </div>
+<section
+	class="flex flex-col items-start justify-center max-w-5xl w-full px-4 mx-auto sm:px-8 py-8 text-black dark:text-white"
+>
+	<h1 class="mb-4 text-3xl font-bold tracking-tight md:text-5xl">Uses</h1>
+
+	<div class="m-4">
+		<h2 class="text-xl font-bold mb-2">Website</h2>
+		<ul class="list-disc">
+			<li><a href="https://svelte.dev/" class="dark:text-blue-400">Svelte</a></li>
+			<li><a href="https://swyxkit.netlify.app/" class="dark:text-blue-400">Swyxkit</a></li>
+			<li><a href="https://vercel.com/" class="dark:text-blue-400">Vercel</a></li>
+		</ul>
+	</div>
+
+	<div class="m-4">
+		<h2 class="text-xl font-bold mb-2">Software</h2>
+		<ul class="list-disc">
+			<li>
+				VSCode (with <a
+					href="https://draculatheme.com/visual-studio-code"
+					class="dark:text-blue-400">Dracula</a
+				> theme)
+			</li>
+			<li><a href="https://iterm2.com/" class="dark:text-blue-400">iTerm2</a></li>
+			<li><a href="https://bear.app/" class="dark:text-blue-400">Bear notes</a></li>
+		</ul>
+	</div>
 </section>
